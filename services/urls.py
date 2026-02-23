c2cac6e34fa9d31d52386ce6062146aa10f1effc
Rida Fatima <ridafatima152004@gmail.com>
2026-02-23 13:37:07 +0530

    changes in settings

--- services/urls.py
+++ services/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('providers/', views.providers, name='providers'),
     path('add-provider/', views.add_provider, name='add_provider'),
 ]
